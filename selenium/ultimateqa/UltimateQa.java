c55321fd27529e1ddf4fc28542c35e42ad81237e
sima <svetislavsimic77@gmail.com>
2018-10-22 19:33:50 +0200

    add log in in UltimateQa

--- selenium/ultimateqa/UltimateQa.java
+++ selenium/ultimateqa/UltimateQa.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.*;
 
+
+
 public class UltimateQa {
 	//https://www.ultimateqa.com/sample-application-lifecycle-sprint-1/
 	public static void fillFirstName() throws InterruptedException {
@@ -52,7 +54,35 @@ public class UltimateQa {
 		submit.click();
 		Thread.sleep(5000);
 		driver.close();	
-	}
+	}	
+	
+	public static void logIn() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
 
+		String url = "https://www.ultimateqa.com/simple-html-elements-for-automation/";
+		driver.get(url);
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("//input[@value='male']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Go to login page')]")).click();;
+		//WebElement firstName = driver.findElement(By.xpath("//input[@name='firstname']"));
+		String currentUrl = driver.getCurrentUrl();
+		String expectedUrl = "https://courses.ultimateqa.com/users/sign_in";
+		if (currentUrl.equals(expectedUrl)){
+			System.out.println("Verification Successful - The correct Url is opened.");
+		}else{
+			System.out.println("Verification Failed - The correct Url isn't opened.");
+		}
+		
+		WebElement email = driver.findElement(By.xpath("//input[@id='user_email']"));
+		email.sendKeys("svetislavsimic77@gmail.com");
+		WebElement password = driver.findElement(By.xpath("//input[@id='user_password']"));
+		password.sendKeys("itbootcamp");
+		Thread.sleep(5000);
+		WebElement submit = driver.findElement(By.xpath("//input[@id='btn-signin']"));
+		submit.click();
+		//WebElement submit = driver.findElement(By.xpath("//form[@action='https://www.ultimateqa.com']//input[@type='submit']"));
+		Thread.sleep(5000);
+		driver.close();	
+	}
 }
 
