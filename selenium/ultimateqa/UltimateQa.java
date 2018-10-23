ba9b48cacd466b4dc5a90f7095e715ebea6d4c81
sima <svetislavsimic77@gmail.com>
2018-10-23 15:38:21 +0200

    added test for incorrect password

--- selenium/ultimateqa/UltimateQa.java
+++ selenium/ultimateqa/UltimateQa.java
@@ -3,15 +3,15 @@ package itbootcamp;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.*;
+
 
 
 
 public class UltimateQa {
 	//https://www.ultimateqa.com/sample-application-lifecycle-sprint-1/
-	public static void fillFirstName() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-
+	private static 	String url = "https://www.ultimateqa.com/simple-html-elements-for-automation/";
+	
+	public static void fillFirstName(WebDriver driver) throws InterruptedException {
 		String url = "https://www.ultimateqa.com/sample-application-lifecycle-sprint-1/";
 		driver.get(url);
 		Thread.sleep(5000);
@@ -21,11 +21,8 @@ public class UltimateQa {
 		WebElement submit = driver.findElement(By.xpath("//input[@id='submitForm']"));
 		submit.click();
 		Thread.sleep(5000);
-		driver.close();
 	}
-	public static void fillFirstAndLastName() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-
+	public static void fillFirstAndLastName(WebDriver driver) throws InterruptedException {
 		String url = "https://www.ultimateqa.com/sample-application-lifecycle-sprint-2/";
 		driver.get(url);
 		Thread.sleep(5000);
@@ -36,12 +33,9 @@ public class UltimateQa {
 		WebElement submit = driver.findElement(By.xpath("//form[@action='https://www.ultimateqa.com']//input[@type='submit']"));
 		submit.click();
 		Thread.sleep(5000);
-		driver.close();	
 	}
 	
-	public static void fillFirstLastNameAndGender() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-
+	public static void fillFirstLastNameAndGender(WebDriver driver) throws InterruptedException {
 		String url = "https://www.ultimateqa.com/sample-application-lifecycle-sprint-3/";
 		driver.get(url);
 		Thread.sleep(5000);
@@ -53,13 +47,9 @@ public class UltimateQa {
 		WebElement submit = driver.findElement(By.xpath("//form[@action='https://www.ultimateqa.com']//input[@type='submit']"));
 		submit.click();
 		Thread.sleep(5000);
-		driver.close();	
 	}	
 	
-	public static void logIn() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-
-		String url = "https://www.ultimateqa.com/simple-html-elements-for-automation/";
+	public static void logIn(WebDriver driver) throws InterruptedException {
 		driver.get(url);
 		Thread.sleep(5000);
 		//driver.findElement(By.xpath("//input[@value='male']")).click();
@@ -82,7 +72,39 @@ public class UltimateQa {
 		submit.click();
 		//WebElement submit = driver.findElement(By.xpath("//form[@action='https://www.ultimateqa.com']//input[@type='submit']"));
 		Thread.sleep(5000);
-		driver.close();	
+	}
+	//
+	public static void logInWithIncorrectPassword(WebDriver driver) throws InterruptedException {
+		driver.get(url);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[contains(text(),'Go to login page')]")).click();;
+		String currentUrl = driver.getCurrentUrl();
+		String expectedUrl = "https://courses.ultimateqa.com/users/sign_in";
+		if (currentUrl.equals(expectedUrl)){
+			System.out.println("Verification Successful - The correct Url is opened.");
+		}else{
+			System.out.println("Verification Failed - The correct Url isn't opened.");
+		}
+		
+		WebElement email = driver.findElement(By.xpath("//input[@id='user_email']"));
+		email.sendKeys("svetislavsimic77@gmail.com");
+		WebElement password = driver.findElement(By.xpath("//input[@id='user_password']"));
+		String wrongPassword = "1234";
+		password.sendKeys(wrongPassword);
+		WebElement submit = driver.findElement(By.xpath("//input[@id='btn-signin']"));
+		submit.click();
+		WebElement errorElement = driver.findElement(By.xpath("//li[@class='notifications-error__list-item']"));
+		String errorMessage = errorElement.getText();
+		String expectedErrorMessage = "Invalid email or password.";
+		if(errorMessage.equals(expectedErrorMessage))
+		{
+			System.out.println("Test pass - The correct error message is showned.");
+		}
+		else
+		{
+			System.out.println("Test failed - The incorrect error message is showned.");
+		}
+		Thread.sleep(5000);
 	}
 }
 
